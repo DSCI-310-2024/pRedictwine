42c61b114a72851aed10082c0eb407cb65380b0c
xnrxng <xinruixiang@hotmail.com>
2024-04-05 01:10:38 -0700

    added functions to R

---
+++ R/create_histogram.R
@@ -0,0 +1,47 @@
+#' create_histogram
+#'
+#' Creates ggplot histograms with x labels, different coloured qualities, and
+#' specifiable x-axis, y-axis, labels bundled into a workflow. It is then applied
+#' to a workflow where the input is training data, and the workflow is used
+#' alongside cowplot commands to create the visualization.
+#'
+#' @param df A training data frame or data frame extension (e.g. a tibble).
+#' @param x The column within data_frame that should serve as the x axis.
+#' @param group The column within data_frame that the histogram should be coloured by.
+#' @param x_lab A string if a label for the x-axis is required. Default is "" (no axis label).
+#' @param y_lab A string if a label for the y-axis is required. Default is "" (no axis label).
+#' @param font_size Size of the font used in the histogram. Default is 10.5.
+#'
+#' @return A histogram created by ggplot.
+#'  The histogram should have different colours based on the quality of wine.
+#'  The x-label should always show, while the y-axis is only shown if add_y_lab = TRUE.
+#'  Returned histogram should work well with cowplot so the histograms are joined as one
+#'  visualization.
+#'
+#' @export
+#'
+#' @examples
+#' create_histogram(df = training, x = chlorides, group = quality,
+#'                  x_lab = "Chlorides", font_size = 10.5)
+#'
+create_histogram <- function(df,
+                             x,
+                             group,
+                             x_lab = "",
+                             y_lab = "",
+                             font_size = 10.5) {
+
+  histogram <- df %>%
+    ggplot2::ggplot(ggplot2::aes(x = {{x}})) +
+    ggplot2::geom_histogram(ggplot2::aes(color = {{group}}, fill = {{group}})) +
+    ggplot2::xlab(x_lab) +
+    ggplot2::ylab(y_lab) +
+    ggplot2::theme(text = ggplot2::element_text(size=font_size),
+                   legend.position="none")
+
+  if(y_lab == "") {
+    histogram <- histogram + ggplot2::theme(axis.title.y = element_blank())
+  }
+
+  return(histogram)
+}
---
+++ R/fit_linear_reg.R
@@ -0,0 +1,32 @@
+#' Fit Linear Regression Model
+#'
+#' Specifies a linear regression model, creates a recipe, and bundles them into
+#' a workflow. Applies workflow to training data to return a fitted model.
+#'
+#' @param formula An R regression formula.
+#' @param data_frame A training data frame or data frame extension (e.g. a tibble).
+#'
+#' @return A fitted linear regression model object.
+#'  Returned object contains finalized recipe and fitted model objects (e.g.,
+#'  coefficients) inside.
+#' @export
+#'
+#' @examples
+#' fit_linear_reg(quality ~ ., training)
+fit_linear_reg <- function(formula, data_frame) {
+  # Specify linear regression
+  lm_spec <- parsnip::linear_reg() %>%
+    parsnip::set_engine("lm") %>%
+    parsnip::set_mode("regression")
+
+  # Set up the recipe
+  wine_lm_recipe <- recipes::recipe(formula, data = data_frame)
+
+  # Training the model.
+  wine_lm_fit <- workflows::workflow() %>%
+    workflows::add_recipe(wine_lm_recipe) %>%
+    workflows::add_model(lm_spec) %>%
+    parsnip::fit(data = data_frame)
+
+  return(wine_lm_fit)
+}
