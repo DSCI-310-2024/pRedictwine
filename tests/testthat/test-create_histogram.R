90ebca7b6dbdd5e41ba14da646b4d18975ad59ec
KevinatorYu <kevinator.yu@hotmail.com>
2024-04-10 18:08:49 -0700

    added a few tests, modified the visualization function slightly

--- tests/testthat/test-create_histogram.R
+++ tests/testthat/test-create_histogram.R
@@ -1,7 +1,26 @@
-library(testthat)
-library(tidyverse)
 library(vdiffr)
-source("../../R/create_histogram.R")
+library(rlang)
+
+test_that("Function should produce an error if incorrect types are passed into each of the arguments", {
+  
+  expect_error(create_histogram(df="yo", x=speed, group=group))
+  
+  plot2 <- create_histogram(df=simple_dataset, x="wassup", group=group)
+  expect_error(print(plot2))
+  
+  plot3 <- create_histogram(df=simple_dataset, x=speed, group=gamers)
+  expect_error(print(plot3))
+  
+  plot4 <- create_histogram(df=simple_dataset, x=speed, group=group, x_lab = df)
+  expect_error(print(plot4))
+  
+  plot5 <- create_histogram(df=simple_dataset, x=speed, group=group, font_size = "no")
+  expect_error(print(plot5))
+}) 
+
+test_that("Function should return a ggplot", {
+  expect_s3_class(simple_histogram, "ggplot")
+})
 
 test_that("Plot should use geom_bar and map x to x-axis, and the grouping are of the same colours", {
   expect_true("GeomBar" %in% c(class(simple_histogram$layers[[1]]$geom)))
@@ -29,11 +48,10 @@ test_that("Default settings plot should have all default values", {
 })
 
 test_that("Colours chosen for the different groups should still remain the same", {
-  expect_equal("#F8766D", unique(ggplot_build(simple_histogram)$data[[1]][,1])[1])
-  expect_equal("#B79F00", unique(ggplot_build(simple_histogram)$data[[1]][,1])[2])
-  expect_equal("#00BA38", unique(ggplot_build(simple_histogram)$data[[1]][,1])[3])
-  expect_equal("#00BFC4", unique(ggplot_build(simple_histogram)$data[[1]][,1])[4])
-  expect_equal("#619CFF", unique(ggplot_build(simple_histogram)$data[[1]][,1])[5])
-  expect_equal("#F564E3", unique(ggplot_build(simple_histogram)$data[[1]][,1])[6])
+  expect_equal("#F8766D", unique(ggplot2::ggplot_build(simple_histogram)$data[[1]][,1])[1])
+  expect_equal("#B79F00", unique(ggplot2::ggplot_build(simple_histogram)$data[[1]][,1])[2])
+  expect_equal("#00BA38", unique(ggplot2::ggplot_build(simple_histogram)$data[[1]][,1])[3])
+  expect_equal("#00BFC4", unique(ggplot2::ggplot_build(simple_histogram)$data[[1]][,1])[4])
+  expect_equal("#619CFF", unique(ggplot2::ggplot_build(simple_histogram)$data[[1]][,1])[5])
+  expect_equal("#F564E3", unique(ggplot2::ggplot_build(simple_histogram)$data[[1]][,1])[6])
 })
-
